ecdc67783afd1fe9635adbffa9920545cf36b40b
palfigergo <palfi.balaton@gmail.com>
2021-01-25 13:32:51 +0100

    feat(BeerController): getBeerById

--- src/main/java/polson/webshop/beers/controllers/BeerController.java
+++ src/main/java/polson/webshop/beers/controllers/BeerController.java
@@ -4,6 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,4 +35,10 @@ public class BeerController {
                 .body(beerDTO);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getBeeryById(@PathVariable("id") Long id) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(beerService.getBeerById(id));
+    }
 }
